8e7a608740042f2c2d8c162a1a650af0bc296926
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2017-06-30 10:27:10 +0100

    Adding Cause and Effect module

---
+++ app/dev/js/components/App.js
@@ -0,0 +1,21 @@
+//Bundles containers
+import React from 'react';
+import UserList from '../containers/user-list';
+import UserDetails from '../containers/user-detail';
+
+require('../../scss/style.scss');
+
+const App = () => (
+    <div>
+    	<h1>Seneca Learning</h1>
+    	<h2>Obesity</h2>
+    	<p>Over 60 percent of U.S. adult women are overweight, according to 2007 estimates from the National Center for Health Statistics of 
+    	the Center for Disease Control and Prevention. Just over one-third of overweight adult women are obese.</p>
+        <h3>Find out more</h3>
+        <UserList />
+        <hr />
+        <UserDetails />
+    </div>
+);
+
+export default App;
---
+++ calorie-convertet/js/calories.js
@@ -0,0 +1,23 @@
+function calsPerDay() {
+
+    function find(id) { 
+        return document.getElementById(id)
+   }
+
+    let age = find("age").value
+    let height = find("height").value * 2.54
+    let weight = find("weight").value / 2.2
+    let result = 0;
+
+    if (find("male").checked) {
+      result = 66.47 + (13.75 * weight) + (5.0 * height - (6.75 * age))
+    }
+
+    else if (find("female").checked) {
+      result = 665.09 + (9.56 * weight) + (1.84 * height - (4.67 * age))
+    }
+
+    find("totalCals").innerHTML = Math.round( result )
+}
+//Run
+calsPerDay()
