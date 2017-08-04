d18503f108812d613f2de00eb9435c1b74ae9c2c
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2017-08-04 15:26:20 +0100

    update index.js

--- making-http/js/actions.js/index.js
+++ making-http/js/actions.js/index.js
@@ -10,15 +10,19 @@ import { reduxHttpMiddleware, GET } from 'redux-http-middleware'
 import axios from 'axios'
  
 // ACTIONS 
+// Takes an objet as argument with 3 keys: 'http verb', 'url', and a call back 'onResponse'
 const getStatus = () => ({
   GET,
   url: 'https://github.com/kukuu/api/stock.json',
   onResponse: 'GET_STATUS_RESPONSE'
 })
  
-// REDUCER 
+// REDUCER => takes 2 arguments::state and action 
+//action has 2 attribute dependencies: type (requuired), and payload which s optional
 const reducer = (state, action) => {
-  if(action.type === 'GET_STATUS_RESPONSE') console.log(`Stock status: ${action.payload.status}`)
+  if(action.type === 'GET_STATUS_RESPONSE') {
+    console.log(`Stock status: ${action.payload.status}`)
+  }
 }
  
 // STORE 
@@ -26,6 +30,9 @@ const config = {
   client: axios
 };
 
+//createStore takes a series of arguments: reducer, applyMiddleware. ApplyMiddleware as a function
+//injects the http middleware 'reduxHttpMiddleware', which attaches the config as a signature dependency
+//the signature- 'config' is passed by the AXIOS client in the chain as a module dependency.
 const store = createStore(
   reducer,
   applyMiddleware(
