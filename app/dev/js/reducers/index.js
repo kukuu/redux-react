c96cda4332349c396a40571a11c6142e1c811c84
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2018-06-22 15:50:08 +0100

    Defining property of combineReducers

--- app/dev/js/reducers/index.js
+++ app/dev/js/reducers/index.js
@@ -4,7 +4,8 @@ import ActiveUserReducer from './reducer-active-user';
 //import bmiReducer from './reducer-bmi';
 
 /*
- * We combine all reducers into a single object before updated data is dispatched (sent) to store
+ * We combine all reducers into a single object before updated data is dispatched (sent) to store - using combineReducers function from REDUX
+ * combineReducers function takes an object as properties.
  * Your entire applications state (store) is just whatever gets returned from all your reducers
  * */
 
