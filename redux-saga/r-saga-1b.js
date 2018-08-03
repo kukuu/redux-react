f35d251571f88d9b34c7e5f0607575381ba6dc9a
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2018-08-03 22:55:26 +0100

    Exporting rootSaga()

---
+++ redux-saga/r-saga-1b.js
@@ -0,0 +1,75 @@
+
+// Author - Alexander Adu-Sarkodie
+//Using redux-saga
+//This is a counter based application
+
+import 'runtime';
+import { createStore, applyMiddleware } from 'redux';
+import { logger } from 'logger-redux';
+import { createSagaMiddleware } from 'redux-saga';
+import { axios } from 'axios';
+
+import { fetchUserWatcher } from './sagas'
+
+let sagaMiddleware = createSagaMiddleware();
+let middleware = applyMiddleware(logger, sagaMiddleware);
+
+let initialState = {
+	counter:0,
+	users: []
+}
+
+//Create Reducer. Which takes 2 arguments satae and  Actions and despatches
+function state( state = initialState, acttion){
+	switch(acttion.type){
+		case 'INCREMENT'://Despaatching increment Action
+		return Object.assign( {}, state , {
+			counter: state.counter + 1
+		});
+
+		case 'DECREMENT'; //Despatching decrement action
+		return Object.assign( {}, state , {
+			counter: state.counter - 1
+		});
+
+		case 'FETCH_USERS_SUCCESS'; //Despatching decrement action
+		return Object.assign( {}, state , {
+			users: action.payload
+		});
+
+
+		default:
+			return state;
+	}
+}
+
+let store = createStore( state , middleware);
+
+//run the root saga middleware. Compose all the multiple watchers if more than one. 
+sagaMiddleware.run(fetchUserWatcher);
+
+//We attache the store to the window and then dispatch
+window.store = store;
+
+//in the dispatch we call our Action creator functions
+// Custom Dispatcher Object we attach to the window :  we attach the reducer to dispatch on the action type
+window.dispatcher = {
+	dispatchIncrement: () => {
+		store.dispatch({ type: 'INCREMENT'})
+	},
+	dispatchDecrement: () => {
+		state.dispatch({ type: 'DECREMENT'})
+	},
+	fetchUser: () => {
+		store.dispatch({ type: 'FETCH_USERS' })
+	}
+	//if there were multiple sagas then you create a root saga
+	export functions* rootSaga(){
+		yield all([
+			fetchUserWatcher(),
+			anotherSaga()
+
+		])
+	}
+}
+
